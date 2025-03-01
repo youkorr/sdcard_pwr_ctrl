e275127bcf0a87b57bba947f692507d719148ca5
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-01 19:42:33 +0100

    Update esp32s3box3_sd_card.h

--- components/sd_card_power/esp32s3box3_sd_card.h
+++ components/sd_card_power/esp32s3box3_sd_card.h
@@ -1,4 +1,4 @@
-#p#pragma once
+#pragma once
 
 #include "esphome/core/component.h"
 #include "esphome/components/sd_mmc_card/sd_mmc_card.h"
@@ -7,24 +7,20 @@
 namespace esphome {
 namespace esp32s3box3_sd_card {
 
-#define SDCARD_PWR_CTRL             GPIO_NUM_43
-#define ESP_SD_PIN_CLK              GPIO_NUM_11
-#define ESP_SD_PIN_CMD              GPIO_NUM_14
-#define ESP_SD_PIN_D0               GPIO_NUM_9
-#define ESP_SD_PIN_D1               GPIO_NUM_13
-#define ESP_SD_PIN_D2               GPIO_NUM_42
-#define ESP_SD_PIN_D3               GPIO_NUM_12
-
 class ESP32S3BOX3SDCard : public Component {
  public:
   void set_sd_mmc_card(sd_mmc_card::SdMmc *sd_mmc) { sd_mmc_ = sd_mmc; }
+  void set_power_pin(int pin) { power_pin_ = pin; }
   void setup() override;
   void dump_config() override;
+  
+  bool is_mounted() const { return sd_mmc_ != nullptr && sd_mmc_->is_mounted(); }
 
  protected:
   void init_power_pin_();
 
   sd_mmc_card::SdMmc *sd_mmc_{nullptr};
+  int power_pin_{-1};
 };
 
 }  // namespace esp32s3box3_sd_card
