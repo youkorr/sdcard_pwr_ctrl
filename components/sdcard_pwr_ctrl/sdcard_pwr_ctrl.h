caeff5201a4beb66da25708d04064606eb561a3b
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-01 17:24:27 +0100

    Update sdcard_pwr_ctrl.h

--- components/sdcard_pwr_ctrl/sdcard_pwr_ctrl.h
+++ components/sdcard_pwr_ctrl/sdcard_pwr_ctrl.h
@@ -15,6 +15,23 @@ class SDCardPowerControl : public Component {
   GPIOPin *power_pin_{nullptr};
 };
 
+// Déclaration du schéma de configuration YAML
+class SDCardPowerControlComponent : public Component {
+ public:
+  void set_power_pin(GPIOPin *pin) { this->power_pin_ = pin; }
+
+  void setup() override {
+    if (this->power_pin_ != nullptr) {
+      this->power_pin_->setup();
+      this->power_pin_->digital_write(true); // Activer l'alimentation (HIGH)
+      ESP_LOGD("SDCardPowerControl", "Alimentation du lecteur SD activée via GPIO43");
+    }
+  }
+
+ protected:
+  GPIOPin *power_pin_{nullptr};
+};
+
 }  // namespace sdcard_pwr_ctrl
 }  // namespace esphome
 
