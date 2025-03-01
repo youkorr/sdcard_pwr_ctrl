17dbef8b298076506ef90794bddaf0d7b5576158
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-02 00:47:36 +0100

    Update sd_card.h

--- components/sd_card/sd_card.h
+++ components/sd_card/sd_card.h
@@ -12,28 +12,29 @@ class SDCard : public Component {
  public:
   void set_clk_pin(GPIOPin *pin) { 
     clk_pin_ = pin; 
-    clk_pin_num_ = pin ? pin->get_gpio() : -1;  // Correction: get_pin() -> get_gpio()
+    clk_pin_num_ = pin ? pin->get_pin() : -1;  // Remplacement de get_gpio() par get_pin()
   }
   void set_cmd_pin(GPIOPin *pin) { 
     cmd_pin_ = pin; 
-    cmd_pin_num_ = pin ? pin->get_gpio() : -1;  // Correction: get_pin() -> get_gpio()
+    cmd_pin_num_ = pin ? pin->get_pin() : -1;  // Remplacement de get_gpio() par get_pin()
   }
   void set_data0_pin(GPIOPin *pin) { 
     data0_pin_ = pin; 
-    data0_pin_num_ = pin ? pin->get_gpio() : -1;  // Correction: get_pin() -> get_gpio()
+    data0_pin_num_ = pin ? pin->get_pin() : -1;  // Remplacement de get_gpio() par get_pin()
   }
   void set_data1_pin(GPIOPin *pin) { 
     data1_pin_ = pin; 
-    data1_pin_num_ = pin ? pin->get_gpio() : -1;  // Correction: get_pin() -> get_gpio()
+    data1_pin_num_ = pin ? pin->get_pin() : -1;  // Remplacement de get_gpio() par get_pin()
   }
   void set_data2_pin(GPIOPin *pin) { 
     data2_pin_ = pin; 
-    data2_pin_num_ = pin ? pin->get_gpio() : -1;  // Correction: get_pin() -> get_gpio()
+    data2_pin_num_ = pin ? pin->get_pin() : -1;  // Remplacement de get_gpio() par get_pin()
   }
-  void set_data3_pin(GPIOPin *pin) {  // Compléter cette fonction qui était coupée
+  void set_data3_pin(GPIOPin *pin) { 
     data3_pin_ = pin; 
-    data3_pin_num_ = pin ? pin->get_gpio() : -1;  // Correction: get_pin() -> get_gpio()
+    data3_pin_num_ = pin ? pin->get_pin() : -1;  // Remplacement de get_gpio() par get_pin()
   }
+
   
   void set_power_pin(GPIOPin *pin) { power_pin_ = pin; }
   void set_mode_1bit(bool mode_1bit) { mode_1bit_ = mode_1bit; }
