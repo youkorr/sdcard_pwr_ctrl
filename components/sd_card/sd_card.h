e19cd88e0331741465a13474dc1ca39f28e523d1
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-01 22:10:24 +0100

    Update sd_card.h

--- components/sd_card/sd_card.h
+++ components/sd_card/sd_card.h
@@ -1,23 +1,21 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "driver/gpio.h"
-#include "driver/sdmmc_host.h"
-#include "driver/sdspi_host.h"
-#include "sdmmc_cmd.h"
+#include "esphome/core/hal.h"
+#include "driver/sdmmc_types.h"
 
 namespace esphome {
 namespace sd_card {
 
 class SDCard : public Component {
  public:
-  void set_clk_pin(int pin) { clk_pin_ = pin; }
-  void set_cmd_pin(int pin) { cmd_pin_ = pin; }
-  void set_data0_pin(int pin) { data0_pin_ = pin; }
-  void set_data1_pin(int pin) { data1_pin_ = pin; }
-  void set_data2_pin(int pin) { data2_pin_ = pin; }
-  void set_data3_pin(int pin) { data3_pin_ = pin; }
-  void set_power_pin(int pin) { power_pin_ = pin; }
+  void set_clk_pin(GPIOPin *pin) { clk_pin_ = pin; }
+  void set_cmd_pin(GPIOPin *pin) { cmd_pin_ = pin; }
+  void set_data0_pin(GPIOPin *pin) { data0_pin_ = pin; }
+  void set_data1_pin(GPIOPin *pin) { data1_pin_ = pin; }
+  void set_data2_pin(GPIOPin *pin) { data2_pin_ = pin; }
+  void set_data3_pin(GPIOPin *pin) { data3_pin_ = pin; }
+  void set_power_pin(GPIOPin *pin) { power_pin_ = pin; }
   void set_mode_1bit(bool mode) { mode_1bit_ = mode; }
 
   void setup() override;
@@ -29,13 +27,13 @@ class SDCard : public Component {
   void init_power_pin_();
   void init_sd_card_();
 
-  int clk_pin_{-1};
-  int cmd_pin_{-1};
-  int data0_pin_{-1};
-  int data1_pin_{-1};
-  int data2_pin_{-1};
-  int data3_pin_{-1};
-  int power_pin_{-1};
+  GPIOPin *clk_pin_{nullptr};
+  GPIOPin *cmd_pin_{nullptr};
+  GPIOPin *data0_pin_{nullptr};
+  GPIOPin *data1_pin_{nullptr};
+  GPIOPin *data2_pin_{nullptr};
+  GPIOPin *data3_pin_{nullptr};
+  GPIOPin *power_pin_{nullptr};
   bool mode_1bit_{true};
   bool mounted_{false};
   sdmmc_card_t *card_{nullptr};
