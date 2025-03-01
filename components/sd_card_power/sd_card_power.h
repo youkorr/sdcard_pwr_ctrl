cd1cb93260b2c7aad1c61072cc3ec616da3fc696
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-01 18:49:36 +0100

    Update sd_card_power.h

--- components/sd_card_power/sd_card_power.h
+++ components/sd_card_power/sd_card_power.h
@@ -2,16 +2,15 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
+#include "driver/gpio.h"
 
 namespace esphome {
 namespace sd_card_power {
 
+#define SDCARD_PWR_CTRL GPIO_NUM_43
+
 class SDCardPower : public Component {
  public:
-  void set_power_pin(GPIOPin *pin) { power_pin_ = pin; }
-  void set_power_on_boot(bool power_on_boot) { power_on_boot_ = power_on_boot; }
-  void set_inverted(bool inverted) { inverted_ = inverted; }
-  
   void setup() override;
   void dump_config() override;
   
@@ -19,16 +18,10 @@ class SDCardPower : public Component {
   void turn_off();
   
  protected:
-  GPIOPin *power_pin_{nullptr};
-  bool power_on_boot_{true};
-  bool inverted_{false};
-  
   bool is_on_{false};
   
-  void write_state_(bool state) {
-    is_on_ = state;
-    this->power_pin_->digital_write(inverted_ ? !state : state);
-  }
+  void configure_gpio_();
+  void set_power_state_(bool state);
 };
 
 }  // namespace sd_card_power
