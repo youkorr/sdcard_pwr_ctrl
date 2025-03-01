96e4759e29374add483db8a2533446b3788a89a8
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-01 19:52:05 +0100

    Update and rename esp32s3box3_sd_card.cpp to sd_card.cpp

--- components/sd_card_power/sd_card.cpp
+++ components/sd_card_power/sd_card.cpp
@@ -1,14 +1,14 @@
-#include "esp32s3box3_sd_card.h"
+#include "sd_card.h"
 #include "esphome/core/log.h"
 #include "esphome/core/hal.h"
 
 namespace esphome {
-namespace esp32s3box3_sd_card {
+namespace sd_card {
 
-static const char *TAG = "esp32s3box3_sd_card";
+static const char *TAG = "sd_card";
 
-void ESP32S3BOX3SDCard::setup() {
-  ESP_LOGCONFIG(TAG, "Setting up ESP32-S3-BOX-3 SD Card...");
+void SDCard::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up SD Card...");
   
   if (this->power_pin_ < 0) {
     ESP_LOGE(TAG, "Power pin not set!");
@@ -26,13 +26,13 @@ void ESP32S3BOX3SDCard::setup() {
   }
 }
 
-void ESP32S3BOX3SDCard::dump_config() {
-  ESP_LOGCONFIG(TAG, "ESP32-S3-BOX-3 SD Card:");
+void SDCard::dump_config() {
+  ESP_LOGCONFIG(TAG, "SD Card:");
   ESP_LOGCONFIG(TAG, "  Power Pin: GPIO%d", this->power_pin_);
   ESP_LOGCONFIG(TAG, "  Mounted: %s", YESNO(this->is_mounted()));
 }
 
-void ESP32S3BOX3SDCard::init_power_pin_() {
+void SDCard::init_power_pin_() {
   gpio_config_t io_conf = {};
   io_conf.intr_type = GPIO_INTR_DISABLE;
   io_conf.mode = GPIO_MODE_OUTPUT;
@@ -45,5 +45,5 @@ void ESP32S3BOX3SDCard::init_power_pin_() {
   ESP_LOGI(TAG, "SD Card power turned ON");
 }
 
-}  // namespace esp32s3box3_sd_card
+}  // namespace sd_card
 }  // namespace esphome
