0c6489a96b61adc5e84716b7f7e8893616281a5a
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-02 00:34:21 +0100

    Update sd_card.h

--- components/sd_card/sd_card.h
+++ components/sd_card/sd_card.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
 #include "driver/sdmmc_types.h"
@@ -11,24 +10,51 @@ class SDCard : public Component {
  public:
   void set_clk_pin(GPIOPin *pin) { 
     clk_pin_ = pin; 
-    clk_pin_num_ = pin ? pin->get_pin() : -1; 
+    clk_pin_num_ = pin ? pin->get_gpio() : -1;  // Correction: get_pin() -> get_gpio()
   }
   void set_cmd_pin(GPIOPin *pin) { 
     cmd_pin_ = pin; 
-    cmd_pin_num_ = pin ? pin->get_pin() : -1; 
+    cmd_pin_num_ = pin ? pin->get_gpio() : -1;  // Correction: get_pin() -> get_gpio()
   }
   void set_data0_pin(GPIOPin *pin) { 
     data0_pin_ = pin; 
-    data0_pin_num_ = pin ? pin->get_pin() : -1; 
+    data0_pin_num_ = pin ? pin->get_gpio() : -1;  // Correction: get_pin() -> get_gpio()
   }
   void set_data1_pin(GPIOPin *pin) { 
     data1_pin_ = pin; 
-    data1_pin_num_ = pin ? pin->get_pin() : -1; 
+    data1_pin_num_ = pin ? pin->get_gpio() : -1;  // Correction: get_pin() -> get_gpio()
   }
   void set_data2_pin(GPIOPin *pin) { 
     data2_pin_ = pin; 
-    data2_pin_num_ = pin ? pin->get_pin() : -1; 
+    data2_pin_num_ = pin ? pin->get_gpio() : -1;  // Correction: get_pin() -> get_gpio()
   }
-  void set_data3_pin(GPIOPin *
+  void set_data3_pin(GPIOPin *pin) {
+    data3_pin_ = pin;
+    data3_pin_num_ = pin ? pin->get_gpio() : -1;  // Correction: get_pin() -> get_gpio()
+  }
+
+  // Déclaration des autres méthodes nécessaires, comme setup(), loop(), dump_config(), etc.
+  void setup() override;
+  void dump_config() override;
+
+ protected:
+  // Déclaration des variables membres
+  GPIOPin *clk_pin_{nullptr};
+  GPIOPin *cmd_pin_{nullptr};
+  GPIOPin *data0_pin_{nullptr};
+  GPIOPin *data1_pin_{nullptr};
+  GPIOPin *data2_pin_{nullptr};
+  GPIOPin *data3_pin_{nullptr};
+  
+  int clk_pin_num_{-1};
+  int cmd_pin_num_{-1};
+  int data0_pin_num_{-1};
+  int data1_pin_num_{-1};
+  int data2_pin_num_{-1};
+  int data3_pin_num_{-1};
+};
+
+}  // namespace sd_card
+}  // namespace esphome
 
 
