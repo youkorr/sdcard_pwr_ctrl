b04e882ae0c52aabf3476c80be1e2813df5771d4
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-01 19:16:44 +0100

    Update __init__.py

--- components/sd_card_power/__init__.py
+++ components/sd_card_power/__init__.py
@@ -2,14 +2,14 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-DEPENDENCIES = []
-CODEOWNERS = ["@votre-nom-utilisateur"]
+DEPENDENCIES = ["esp32"]
+AUTO_LOAD = ["sdcard"]
 
-sd_card_power_ns = cg.esphome_ns.namespace("sd_card_power")
-SDCardPower = sd_card_power_ns.class_("SDCardPower", cg.Component)
+esp32s3box3_sd_card_ns = cg.esphome_ns.namespace("esp32s3box3_sd_card")
+ESP32S3BOX3SDCard = esp32s3box3_sd_card_ns.class_("ESP32S3BOX3SDCard", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(SDCardPower),
+    cv.GenerateID(): cv.declare_id(ESP32S3BOX3SDCard),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
