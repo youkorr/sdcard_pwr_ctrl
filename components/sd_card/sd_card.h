4e3529efaa3b760dcc66009e420011588de73351
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-01 23:56:03 +0100

    Update sd_card.h

--- components/sd_card/sd_card.h
+++ components/sd_card/sd_card.h
@@ -11,28 +11,29 @@ class SDCard : public Component {
  public:
   void set_clk_pin(GPIOPin *pin) {
     clk_pin_ = pin;
-    clk_pin_num_ = pin ? pin->get_pin() : -1;
+    clk_pin_num_ = pin ? pin->get_gpio() : -1;
   }
   void set_cmd_pin(GPIOPin *pin) {
     cmd_pin_ = pin;
-    cmd_pin_num_ = pin ? pin->get_pin() : -1;
+    cmd_pin_num_ = pin ? pin->get_gpio() : -1;
   }
   void set_data0_pin(GPIOPin *pin) {
     data0_pin_ = pin;
-    data0_pin_num_ = pin ? pin->get_pin() : -1;
+    data0_pin_num_ = pin ? pin->get_gpio() : -1;
   }
   void set_data1_pin(GPIOPin *pin) {
     data1_pin_ = pin;
-    data1_pin_num_ = pin ? pin->get_pin() : -1;
+    data1_pin_num_ = pin ? pin->get_gpio() : -1;
   }
   void set_data2_pin(GPIOPin *pin) {
     data2_pin_ = pin;
-    data2_pin_num_ = pin ? pin->get_pin() : -1;
+    data2_pin_num_ = pin ? pin->get_gpio() : -1;
   }
   void set_data3_pin(GPIOPin *pin) {
     data3_pin_ = pin;
-    data3_pin_num_ = pin ? pin->get_pin() : -1;
+    data3_pin_num_ = pin ? pin->get_gpio() : -1;
   }
+
 
 
   void setup() override;
