d38db73fcdca6fb76e575811dd067e8ab80415be
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-01 21:51:47 +0100

    Update sd_card.h

--- components/sd_card_power/sd_card.h
+++ components/sd_card_power/sd_card.h
@@ -1,26 +1,44 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/sd_mmc_card/sd_mmc_card.h"
 #include "driver/gpio.h"
+#include "driver/sdmmc_host.h"
+#include "driver/sdspi_host.h"
+#include "sdmmc_cmd.h"
 
 namespace esphome {
 namespace sd_card {
 
 class SDCard : public Component {
  public:
-  void set_sd_mmc_card(sd_mmc_card::SdMmc *sd_mmc) { sd_mmc_ = sd_mmc; }
+  void set_clk_pin(int pin) { clk_pin_ = pin; }
+  void set_cmd_pin(int pin) { cmd_pin_ = pin; }
+  void set_data0_pin(int pin) { data0_pin_ = pin; }
+  void set_data1_pin(int pin) { data1_pin_ = pin; }
+  void set_data2_pin(int pin) { data2_pin_ = pin; }
+  void set_data3_pin(int pin) { data3_pin_ = pin; }
   void set_power_pin(int pin) { power_pin_ = pin; }
+  void set_mode_1bit(bool mode) { mode_1bit_ = mode; }
+
   void setup() override;
   void dump_config() override;
   
-  bool is_mounted() const { return sd_mmc_ != nullptr && sd_mmc_->is_mounted(); }
+  bool is_mounted() const { return mounted_; }
 
  protected:
   void init_power_pin_();
+  void init_sd_card_();
 
-  sd_mmc_card::SdMmc *sd_mmc_{nullptr};
+  int clk_pin_{-1};
+  int cmd_pin_{-1};
+  int data0_pin_{-1};
+  int data1_pin_{-1};
+  int data2_pin_{-1};
+  int data3_pin_{-1};
   int power_pin_{-1};
+  bool mode_1bit_{true};
+  bool mounted_{false};
+  sdmmc_card_t *card_{nullptr};
 };
 
 }  // namespace sd_card
