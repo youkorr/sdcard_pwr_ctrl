69cddaa635ede8cf8563f7567e70d22e0224d064
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-03 23:30:51 +0100

    Update sd_card_power.cpp

--- components/sd_card_power/sd_card_power.cpp
+++ components/sd_card_power/sd_card_power.cpp
@@ -4,21 +4,21 @@ namespace esphome {
 namespace sd_card_power {
 
 void SDCardPower::setup() {
-  if (SDCARD_PWR_CTRL >= 0) {
+  if (power_ctrl_pin_ >= 0) {
     gpio_config_t gpio_cfg = {
-      .pin_bit_mask = 1ULL << SDCARD_PWR_CTRL,
+      .pin_bit_mask = 1ULL << power_ctrl_pin_,
       .mode = GPIO_MODE_OUTPUT,
       .pull_up_en = GPIO_PULLUP_DISABLE,
       .pull_down_en = GPIO_PULLDOWN_DISABLE,
       .intr_type = GPIO_INTR_DISABLE
     };
     gpio_config(&gpio_cfg);
-    gpio_set_level(SDCARD_PWR_CTRL, 1); // Active l'alimentation de la carte SD
+    gpio_set_level(power_ctrl_pin_, 1); // Active l'alimentation de la carte SD
   }
 }
 
 void SDCardPower::set_power(bool state) {
-  gpio_set_level(SDCARD_PWR_CTRL, state ? 1 : 0);
+  gpio_set_level(power_ctrl_pin_, state ? 1 : 0);
 }
 
 }  // namespace sd_card_power
