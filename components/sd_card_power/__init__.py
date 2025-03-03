9a63d7f046c8e043aebb94b4bafd1dd7d19b8078
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-03 23:25:17 +0100

    Update __init__.py

--- components/sd_card_power/__init__.py
+++ components/sd_card_power/__init__.py
@@ -3,15 +3,16 @@ import esphome.config_validation as cv
 from esphome.components import gpio
 from esphome.const import CONF_ID
 
-pwr_ctrl_sd_ns = cg.esphome_ns.namespace('pwr_ctrl_sd')
-PwrCtrlSD = pwr_ctrl_sd_ns.class_('PwrCtrlSD', cg.Component)
+sd_card_power_ns = cg.esphome_ns.namespace('sd_card_power')
+SDCardPower = sd_card_power_ns.class_('SDCardPower', cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(PwrCtrlSD),
+    cv.GenerateID(): cv.declare_id(SDCardPower),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+
 
 
