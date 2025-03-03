9e29187c695e7156008bd239d1c09de0db040b69
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-03 23:26:17 +0100

    Update sd_card_power.h

--- components/sd_card_power/sd_card_power.h
+++ components/sd_card_power/sd_card_power.h
@@ -1,34 +1,19 @@
 #pragma once
 
-#include "esphome/core/component.h"
-#include "esphome/core/hal.h"
+#include "esphome.h"
 #include "driver/gpio.h"
+
+#define SDCARD_PWR_CTRL GPIO_NUM_43
 
 namespace esphome {
 namespace sd_card_power {
 
-#define SDCARD_PWR_CTRL GPIO_NUM_43
-#define SDCARD_INTR_GPIO GPIO_NUM_44
-#define SDCARD_OPEN_FILE_NUM_MAX 5
-
 class SDCardPower : public Component {
  public:
   void setup() override;
-  void dump_config() override;
-  
-  void turn_on();
-  void turn_off();
-  
-  int8_t get_sdcard_intr_gpio() { return SDCARD_INTR_GPIO; }
-  int8_t get_sdcard_open_file_num_max() { return SDCARD_OPEN_FILE_NUM_MAX; }
-  int8_t get_sdcard_power_ctrl_gpio() { return SDCARD_PWR_CTRL; }
-
- protected:
-  bool is_on_{false};
-  
-  void configure_gpio_();
-  void set_power_state_(bool state);
+  void set_power(bool state);
 };
 
 }  // namespace sd_card_power
 }  // namespace esphome
+
