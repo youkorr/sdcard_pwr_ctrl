7210f467338ce4e1b57e0278a0ec9f17a2c05ab8
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-04 00:58:34 +0100

    Update __init__.py

--- components/sd_card_power/__init__.py
+++ components/sd_card_power/__init__.py
@@ -1,22 +1,28 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_PIN
 import esphome.components.esp32 as esp32
 
 CONF_POWER_CTRL_PIN = 'power_ctrl_pin'
 DEPENDENCIES = ['esp32']
 
-sd_card_power_ns = cg.esphome_ns.namespace('sd_card_power')
-SDCardPower = sd_card_power_ns.class_('SDCardPower', cg.Component)
+sdmmc_card_ns = cg.esphome_ns.namespace('sd_mmc_card')
+SdMmc = sdmmc_card_ns.class_('SdMmc', cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(SDCardPower),
-    cv.Optional(CONF_POWER_CTRL_PIN, default=-1): cv.int_range(min=-1, max=48),
+    cv.GenerateID(): cv.declare_id(SdMmc),
+    cv.Optional(CONF_POWER_CTRL_PIN): cv.gpio_pin,
+    # Autres options existantes de configuration
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    cg.add(var.set_power_ctrl_pin(config[CONF_POWER_CTRL_PIN]))
+    
+    # Ajout de la configuration du pin de contrôle de puissance
+    if CONF_POWER_CTRL_PIN in config:
+        pin = config[CONF_POWER_CTRL_PIN]
+        cg.add(var.set_power_ctrl_pin(pin))
+    
     yield cg.register_component(var, config)
 
 
