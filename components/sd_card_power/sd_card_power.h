b1d947699ea1041a185256bd4ae0d7ca67928f5a
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-04 00:16:06 +0100

    Update sd_card_power.h

--- components/sd_card_power/sd_card_power.h
+++ components/sd_card_power/sd_card_power.h
@@ -13,7 +13,10 @@ class SDCardPower : public Component {
   void setup() override;
   void set_power(bool state);
   
-  void set_power_ctrl_pin(int pin) { this->power_ctrl_pin_ = pin; }
+  void set_power_ctrl_pin(int pin) { 
+    this->power_ctrl_pin_ = pin; 
+    ESP_LOGI("SDCardPower", "Power control pin set to %d", pin);
+  }
   
  protected:
   int power_ctrl_pin_{-1};
