6e89ed0c7144049ee03c1b6b56157118cda3762e
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-04 00:15:04 +0100

    Update __init__.py

--- components/sd_card_power/__init__.py
+++ components/sd_card_power/__init__.py
@@ -4,6 +4,7 @@ from esphome.const import CONF_ID
 import esphome.components.esp32 as esp32
 
 CONF_POWER_CTRL_PIN = 'power_ctrl_pin'
+DEPENDENCIES = ['esp32']
 
 sd_card_power_ns = cg.esphome_ns.namespace('sd_card_power')
 SDCardPower = sd_card_power_ns.class_('SDCardPower', cg.Component)
@@ -13,12 +14,10 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_POWER_CTRL_PIN): cv.int_range(min=0, max=48),  # Plage des pins GPIO ESP32
 }).extend(cv.COMPONENT_SCHEMA)
 
-async def to_code(config):
+def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    
-    pin = config[CONF_POWER_CTRL_PIN]
-    cg.add(var.set_power_ctrl_pin(pin))
+    cg.add(var.set_power_ctrl_pin(config[CONF_POWER_CTRL_PIN]))
+    yield cg.register_component(var, config)
 
 
 
