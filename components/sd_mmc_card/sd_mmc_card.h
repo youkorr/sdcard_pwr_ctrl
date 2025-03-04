1c243cbccd3420639b648c9ff46248fc2fbff856
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-04 03:10:25 +0100

    Update sd_mmc_card.h

--- components/sd_mmc_card/sd_mmc_card.h
+++ components/sd_mmc_card/sd_mmc_card.h
@@ -13,27 +13,14 @@ class SdMmc : public Component {
   void setup() override;
   void dump_config() override;
 
-  void set_clk_pin(int8_t pin) { clk_pin_ = pin; }
-  void set_cmd_pin(int8_t pin) { cmd_pin_ = pin; }
-  void set_data0_pin(int8_t pin) { data0_pin_ = pin; }
-  void set_data1_pin(int8_t pin) { data1_pin_ = pin; }
-  void set_data2_pin(int8_t pin) { data2_pin_ = pin; }
-  void set_data3_pin(int8_t pin) { data3_pin_ = pin; }
-  void set_mode_1bit(bool mode) { mode_1bit_ = mode; }
-  void set_power_ctrl_pin(int8_t pin) { power_ctrl_pin_ = pin; } // Ajout de set_power_ctrl_pin
+  void set_power_ctrl_pin(int8_t pin) { power_ctrl_pin_ = pin; }  // Garder uniquement power_ctrl_pin
 
  protected:
-  int8_t clk_pin_{-1};
-  int8_t cmd_pin_{-1};
-  int8_t data0_pin_{-1};
-  int8_t data1_pin_{-1};
-  int8_t data2_pin_{-1};
-  int8_t data3_pin_{-1};  // Assure-toi qu'il est bien là
-  int8_t power_ctrl_pin_{-1};  // Assure-toi qu'il est bien là
-  bool mode_1bit_{false};
+  int8_t power_ctrl_pin_{-1};  // Garder uniquement power_ctrl_pin
 };
 
 }  // namespace sd_mmc_card
 }  // namespace esphome
+
 
    
