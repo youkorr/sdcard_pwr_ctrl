01b59c50092f343971c9c62ed95bb0d3dea90424
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-04 01:11:37 +0100

    Update __init__.py

--- components/sd_card_power/__init__.py
+++ components/sd_card_power/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_PIN
+from esphome.const import CONF_ID
 import esphome.components.esp32 as esp32
 
 CONF_POWER_CTRL_PIN = 'power_ctrl_pin'
@@ -9,27 +9,34 @@ DEPENDENCIES = ['esp32']
 sdmmc_card_ns = cg.esphome_ns.namespace('sd_mmc_card')
 SdMmc = sdmmc_card_ns.class_('SdMmc', cg.Component)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(SdMmc),
-    cv.Required('clk_pin'): cv.gpio_pin_number,
-    cv.Required('cmd_pin'): cv.gpio_pin_number,
-    cv.Required('data0_pin'): cv.gpio_pin_number,
-    cv.Optional('data1_pin'): cv.gpio_pin_number,
-    cv.Optional('data2_pin'): cv.gpio_pin_number,
-    cv.Optional('data3_pin'): cv.gpio_pin_number,
-    cv.Optional('power_ctrl_pin'): cv.gpio_pin_number,  # Ajout optionnel
-    cv.Optional('mode_1bit', default=False): cv.boolean,
-}).extend(cv.COMPONENT_SCHEMA)
+def validate_sd_mmc_config(config):
+    # Ajout de la validation pour power_ctrl_pin
+    if 'power_ctrl_pin' in config:
+        config['power_ctrl_pin'] = cv.gpio_pin_number(config['power_ctrl_pin'])
+    return config
+
+CONFIG_SCHEMA = cv.All(
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(SdMmc),
+        cv.Required('clk_pin'): cv.gpio_pin_number,
+        cv.Required('cmd_pin'): cv.gpio_pin_number,
+        cv.Required('data0_pin'): cv.gpio_pin_number,
+        cv.Optional('data1_pin'): cv.gpio_pin_number,
+        cv.Optional('data2_pin'): cv.gpio_pin_number,
+        cv.Optional('data3_pin'): cv.gpio_pin_number,
+        cv.Optional(CONF_POWER_CTRL_PIN): cv.gpio_pin_number,
+        cv.Optional('mode_1bit', default=False): cv.boolean,
+    }),
+    validate_sd_mmc_config
+)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     
-    # Pins requis
     cg.add(var.set_clk_pin(config['clk_pin']))
     cg.add(var.set_cmd_pin(config['cmd_pin']))
     cg.add(var.set_data0_pin(config['data0_pin']))
     
-    # Pins optionnels
     if 'data1_pin' in config:
         cg.add(var.set_data1_pin(config['data1_pin']))
     if 'data2_pin' in config:
@@ -37,11 +44,9 @@ def to_code(config):
     if 'data3_pin' in config:
         cg.add(var.set_data3_pin(config['data3_pin']))
     
-    # Pin de contrôle de puissance
-    if 'power_ctrl_pin' in config:
-        cg.add(var.set_power_ctrl_pin(config['power_ctrl_pin']))
+    if CONF_POWER_CTRL_PIN in config:
+        cg.add(var.set_power_ctrl_pin(config[CONF_POWER_CTRL_PIN]))
     
-    # Mode 1 bit
     cg.add(var.set_mode_1bit(config.get('mode_1bit', False)))
     
     yield cg.register_component(var, config)
