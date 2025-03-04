d22f7e09fc8d95f78db20e79ff438e4d31840f01
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-04 04:18:32 +0100

    Update pwr_ctrl_esp_idf.cpp

--- components/pwr_ctrl/pwr_ctrl_esp_idf.cpp
+++ components/pwr_ctrl/pwr_ctrl_esp_idf.cpp
@@ -1,26 +1,13 @@
 #include "pwr_ctrl.h"
-
-
-#ifdef USE_ESP_IDF
-
-#include "esp_vfs.h"
-#include "esp_vfs_fat.h"
-#include "sdmmc_cmd.h"
-#include "driver/sdmmc_host.h"
-#include "driver/gpio.h"
-#include "esphome/core/log.h"
+#include "esp_log.h"
+#include "driver/gpio.h"  // Pour la gestion GPIO spécifique à ESP-IDF
 
 namespace esphome {
 namespace pwr_ctrl {
 
-// Définir un TAG pour les logs
-static const char *TAG = "pwr_ctrl";
-
-// Fonction setup pour initialiser le contrôle de l'alimentation de la carte SD
-void SdMmcPowerCtrl::setup() {
-  // Vérifie si une broche de contrôle d'alimentation est définie
+void SdMmcPowerCtrl::setup_idf_specific() {
+  // La logique spécifique à ESP-IDF pour initialiser l'alimentation de la carte SD
   if (this->power_ctrl_pin_ >= 0) {
-    // Configuration du GPIO pour contrôler l'alimentation de la carte SD
     gpio_config_t gpio_cfg = {
       .pin_bit_mask = 1ULL << this->power_ctrl_pin_,
       .mode = GPIO_MODE_OUTPUT,
@@ -28,16 +15,14 @@ void SdMmcPowerCtrl::setup() {
       .pull_down_en = GPIO_PULLDOWN_DISABLE,
       .intr_type = GPIO_INTR_DISABLE
     };
-    // Applique la configuration du GPIO
     gpio_config(&gpio_cfg);
-    // Mettez la broche à l'état bas (désactive l'alimentation)
-    gpio_set_level(static_cast<gpio_num_t>(this->power_ctrl_pin_), 0);
-    // Log d'information
-    ESP_LOGI(TAG, "SD Card power control enabled on GPIO %d", this->power_ctrl_pin_);
+    gpio_set_level(static_cast<gpio_num_t>(this->power_ctrl_pin_), 0);  // Logique spécifique à ESP-IDF
+    ESP_LOGI("sd_mmc_card", "SD Card power control enabled on GPIO %d", this->power_ctrl_pin_);
   }
 }
 
 }  // namespace pwr_ctrl
 }  // namespace esphome
+
 
 #endif  // USE_ESP_IDF
