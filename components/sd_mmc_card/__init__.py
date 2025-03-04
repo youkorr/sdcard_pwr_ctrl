b808e06b07a0ec665a410cd024de5ee83f549542
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-04 01:16:17 +0100

    Create __init__.py

---
+++ components/sd_mmc_card/__init__.py
@@ -0,0 +1,42 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+
+DEPENDENCIES = ['esp32']
+MULTI_CONF = True
+
+sd_mmc_card_ns = cg.esphome_ns.namespace('sd_mmc_card')
+SdMmc = sd_mmc_card_ns.class_('SdMmc', cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(SdMmc),
+    cv.Required('clk_pin'): cv.gpio_pin_number,
+    cv.Required('cmd_pin'): cv.gpio_pin_number,
+    cv.Required('data0_pin'): cv.gpio_pin_number,
+    cv.Optional('data1_pin'): cv.gpio_pin_number,
+    cv.Optional('data2_pin'): cv.gpio_pin_number,
+    cv.Optional('data3_pin'): cv.gpio_pin_number,
+    cv.Optional('power_ctrl_pin'): cv.gpio_pin_number,
+    cv.Optional('mode_1bit', default=False): cv.boolean,
+}).extend(cv.COMPONENT_SCHEMA)
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    
+    cg.add(var.set_clk_pin(config['clk_pin']))
+    cg.add(var.set_cmd_pin(config['cmd_pin']))
+    cg.add(var.set_data0_pin(config['data0_pin']))
+    
+    if 'data1_pin' in config:
+        cg.add(var.set_data1_pin(config['data1_pin']))
+    if 'data2_pin' in config:
+        cg.add(var.set_data2_pin(config['data2_pin']))
+    if 'data3_pin' in config:
+        cg.add(var.set_data3_pin(config['data3_pin']))
+    
+    if 'power_ctrl_pin' in config:
+        cg.add(var.set_power_ctrl_pin(config['power_ctrl_pin']))
+    
+    cg.add(var.set_mode_1bit(config.get('mode_1bit', False)))
+    
+    yield cg.register_component(var, config)
